11f40b071e08ed61424fa846bd1d9b12390950b5
Jarvis <gabrielnatalli12112002@gmail.com>
2025-12-07 15:23:30 -0300

    Atualização 1

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/contracts/deployments.sepolia.json',
+          dest: 'contracts'
+        },
+        {
+          src: 'src/contracts/deployments.localhost.json',
+          dest: 'contracts'
+        }
+      ]
+    })
+  ],
   server: {
     proxy: {
       '/api': {
